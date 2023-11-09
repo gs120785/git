2da21dcee417f3eef314b40b8d5a5d03d28c1d12
gs120785 <144424335+gs120785@users.noreply.github.com>
2023-11-09 14:20:30 +0530

    Add files via upload

---
+++ index_files/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('q5x9us9l') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.uclick = "q5x9us9l"; 
